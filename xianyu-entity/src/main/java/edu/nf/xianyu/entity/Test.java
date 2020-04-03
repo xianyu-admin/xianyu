08baabee1624720404c14d35fadf120f03e8508c
zengxl <934734463@qq.com>
2020-04-03 11:13:39 +0800

    commit

---
+++ xianyu-entity/src/main/java/edu/nf/xianyu/entity/Test.java
@@ -0,0 +1,6 @@
+package edu.nf.xianyu.entity;
+
+public class Test {
+    private String name;
+    private Integer age;
+}
--- xianyu-web/src/main/java/edu/nf/xianyu/controller/vo/ResponseVo.java
+++ xianyu-web/src/main/java/edu/nf/xianyu/controller/vo/ResponseVo.java
@@ -12,4 +12,28 @@ public class ResponseVo<T> {
     private Integer code;
     private Object message;
     private T data;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Object getMessage() {
+        return message;
+    }
+
+    public void setMessage(Object message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
